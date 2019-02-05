95a72f7b3d9ed0c98fcde9dcf18d7461b62ef26d
Antoine Richeux <a.richeux@beapp.fr>
2019-02-05 16:40:59 +0100

    remove cache manager and add 3 examples

--- Example/BeappCache/ViewController.swift
+++ Example/BeappCache/ViewController.swift
@@ -12,23 +12,66 @@ import RxSwift
 
 class ViewController: UIViewController {
     
+    // MARK: - Variables
     let bag = DisposeBag()
+    let cacheManager = RxCacheManager(storageType: .Cache)
+    let singleString = Single.just("Test to save data with Cache library")
     
+    // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        saveDataToCache()
+        asyncRequestOrCache()
+        justAsyncRequest()
+        getDataFromCache()
     }
     
-    func saveDataToCache() {
-        CacheManager.shared.saveData(text: "Test to save data with cache library")
+    // MARK: - Local functions
+    
+    /// Call async request or get data from cache if present
+    func asyncRequestOrCache() {
+        cacheManager.fromKey(key: "key_xxx")
+            .withAsync(singleString)
+            .withStrategy(.asyncOrCache)
+            .fetch()
             .observeOn(MainScheduler.instance)
-            .subscribe(onNext: { (email) in
-                print(email)
+            .subscribe(onNext: { (_) in
+                print("Success async request or data from cache")
             }, onError: { (error) in
                 print(error)
             })
             .disposed(by: bag)
     }
+    
+    /// Call only async request
+    func justAsyncRequest() {
+        cacheManager.fromKey(key: "key_xxx")
+            .withAsync(singleString)
+            .withStrategy(.justAsync)
+            .fetch()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (_) in
+                print("Success async request")
+            }, onError: { (error) in
+                print(error)
+            })
+            .disposed(by: bag)
+    }
+    
+    /// Onlye get data from cache if present
+    func getDataFromCache() {
+        cacheManager.fromKey(key: "key_xxx")
+            .withAsync(singleString)
+            .withStrategy(.justCache)
+            .fetch()
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { (_) in
+                print("Success to get data from cache")
+            }, onError: { (error) in
+                print(error)
+            })
+            .disposed(by: bag)
+    }
+    
 }
 
