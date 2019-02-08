b9b5736c0fed618e172fc47d8c25d12d57eb6d9b
Antoine Richeux <a.richeux@beapp.fr>
2019-02-08 17:44:25 +0100

    Wait the end of asynchronous request to check if the key exist

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -10,7 +10,7 @@ class Tests: XCTestCase {
     override func setUp() {
         super.setUp()
 
-        cacheManager.clear()
+        clearDatabase()
     }
     
     override func tearDown() {
@@ -18,45 +18,54 @@ class Tests: XCTestCase {
         super.tearDown()
     }
     
-    func test_a_CreateKeyInCache() {
+    func testCreateKeyInCache() {
         setDataInCache(forKey: "key_1", strategy: .justAsync)
+        XCTAssertTrue(cacheManager.exist(forKey: "key_1"))
         setDataInCache(forKey: "key_2", strategy: .asyncOrCache)
+        XCTAssertTrue(cacheManager.exist(forKey: "key_2"))
         setDataInCache(forKey: "key_3", strategy: .cacheThenAsync)
+        XCTAssertTrue(cacheManager.exist(forKey: "key_3"))
         setDataInCache(forKey: "key_4", strategy: .cacheOrAsync)
+        XCTAssertTrue(cacheManager.exist(forKey: "key_4"))
     }
 
-    func test_c_GetKeyValueFromCache() {
+    func testGetKeyValueFromCache() {
         getDataFromCache(forKey: "key_1")
         getDataFromCache(forKey: "key_2")
         getDataFromCache(forKey: "key_3")
         getDataFromCache(forKey: "key_4")
     }
     
-    func test_d_DeleteKey() {
+    func testDeleteKey() {
         cacheManager.delete(forKey: "key_1")
         XCTAssertFalse(cacheManager.exist(forKey: "key_1"))
+        clearDatabase()
     }
 
-    func test_e_ClearDatabase() {
+    private func clearDatabase() {
         cacheManager.clear()
         XCTAssertFalse(cacheManager.exist(forKey: "key_2"))
     }
     
     private func setDataInCache(forKey key: String, strategy: CacheStrategy) {
+        let expectation = XCTestExpectation(description: "Download apple.com home page")
+        
         cacheManager.fromKey(key: key)
             .withAsync(Single.just("Data for key \(key) is saved"))
             .withStrategy(strategy)
             .fetch()
             .observeOn(MainScheduler.instance)
             .subscribe(onNext: { (result) in
-                XCTAssertTrue(self.cacheManager.exist(forKey: key))
-                XCTAssert(true)
                 print("Data is created for key \(key)")
             }, onError: { (error) in
                 print("[ERROR] Cannot add the key with error \(error)")
                 XCTAssert(false)
+            }, onCompleted: {
+                expectation.fulfill()
             })
             .disposed(by: bag)
+        
+        wait(for: [expectation], timeout: 2.0)
     }
     
     private func getDataFromCache(forKey key: String) {
@@ -72,6 +81,13 @@ class Tests: XCTestCase {
                 XCTAssert(false)
             })
             .disposed(by: bag)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure() {
+            // Put the code you want to measure the time of here.
+        }
     }
     
 }
