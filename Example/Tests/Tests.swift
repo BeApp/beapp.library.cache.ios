050cb0b72b5f1f552e60cb666957021a5f6c4afd
Antoine Richeux <a.richeux@beapp.fr>
2019-02-08 17:33:16 +0100

    Add code coverage on beappCache librairy only Coverage 87%

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -74,11 +74,4 @@ class Tests: XCTestCase {
             .disposed(by: bag)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
