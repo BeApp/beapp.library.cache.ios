5e2df0866d1866edf52913604955b7705eccea05
Antoine Richeux <a.richeux@beapp.fr>
2019-02-08 17:58:03 +0100

    add new test to test cache with config custom

---
+++ Example/Tests/Tests_Cache_Custom.swift
@@ -0,0 +1,37 @@
+//
+//  Tests_Cache_Custom.swift
+//  BeappCache_Tests
+//
+//  Created by Antoine Richeux on 08/02/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import RxSwift
+import BeappCache
+
+class Tests_Cache_Custom: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+
+    }
+
+    override func tearDown() {
+        super.tearDown()
+        
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
